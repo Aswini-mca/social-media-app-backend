b27a12c0ba210485bf7a1bd627cb90d08d9519bf
Aswini-mca <aswini25.mca@gmail.com>
2023-10-26 19:12:54 +0530

    updated mongo_url

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ export async function dataBaseConnection(){
         useUnifiedTopology:true
     };
     try {
-        mongoose.connect("mongodb://127.0.0.1:27017",params)
+        mongoose.connect(process.env.MONGO_URL,params)
         console.log("Mongodb is connected")
     } catch (error) {
         console.log("Mongodb connection error")
